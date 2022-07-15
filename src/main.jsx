4bdb5935964e9f8e2663d64f9e0e42b40d5e27c2
Pau Egea <papau84@gmail.com>
2022-07-15 12:33:06 +0200

    counter

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom/client';
+import { CounterApp } from './CounterApp';
 import { FirstApp } from './FirstApp';
 // import { HelloWorldApp } from './HelloWorldApp';
 
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         {/* <HelloWorldApp /> */}
         <FirstApp />
+        <CounterApp value = {10}/>
     </React.StrictMode>
 );
 
