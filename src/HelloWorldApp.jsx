6d3e8584ec35d52d0012c0da4e7d97bbcb92c5a1
Pau Egea <papau84@gmail.com>
2022-07-15 11:07:29 +0200

    first component

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,18 @@
+// function App() {
+//     //document.getElement...
+//     return (
+//     <h1>Hola Mundo!!! Como vas?</h1>
+//     );
+// }
+
+// export default App;
+
+
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>Cambiando cositas</h1>
+  )
+}
+
+
