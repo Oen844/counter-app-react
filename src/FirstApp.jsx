3b260792729fdc10d61c5c442724ee32a9a26639
Pau Egea <papau84@gmail.com>
2022-07-15 11:49:03 +0200

    styles

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,22 +1,9 @@
-// const newMessage = {
-//     message: 'Hola mundo',
-//     id: 123454,
-    
-// };
-const getResult = ( a, b) => {
-    return a + b;
-}
-const saludo = () => {
-    return (
-       "Hola mundo"
-    )
-}
 
 export const FirstApp = () => {
   
   return (
     <>
-      <h1>{getResult( 5, 5)}</h1>
+      <h1>Hola Pau</h1>
       <p>This is the first app</p>
     </>
   );
