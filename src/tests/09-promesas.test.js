599ec2dfae07fe4477f73d3267974ad134a3b782
Pau Egea <papau84@gmail.com>
2022-07-17 10:58:56 +0200

    test promesas

---
+++ src/tests/09-promesas.test.js
@@ -0,0 +1,34 @@
+import { getHeroeByIdAsync } from "../base-pruebas/09-promesas";
+import heroes from '../data/heroes';
+
+describe("Pruebas en 09-promesas", () => {
+  test("getHeroeById debe de retornar un heroe por ID", (done) => {
+    const id = 1;
+    getHeroeByIdAsync(id).then((hero) => {
+      expect(hero).toEqual({
+        id: 1,
+        name: "Batman",
+        owner: "DC",
+      });
+
+      done();
+    });
+  });
+
+  test("getHeroeById debe de retornar un error si el heroe no existe", (done) => {
+    const id = heroes.length +1;
+    getHeroeByIdAsync(id)
+      .then((hero) => {
+        expect(hero).toBeFalsy();
+
+        done();
+      })
+      .catch((error) => {
+        console.log(error);
+
+        expect(error).toBe("No se pudo encontrar el héroe con el id: " + id);
+
+        done();
+      });
+  });
+});
