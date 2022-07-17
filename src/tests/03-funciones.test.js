98c71ea3f97e4b7fbd63f6cda92551b197601fe4
Pau Egea <papau84@gmail.com>
2022-07-17 09:15:08 +0200

    test funciones

---
+++ src/tests/03-funciones.test.js
@@ -0,0 +1,26 @@
+import { getUser, getUsuarioActivo } from "../base-pruebas/05-funciones";
+
+describe("Pruebas en 05-funciones", () => {
+  test("getUser debe de retornar un objeto", () => {
+    const testUser = {
+      uid: "ABC123",
+      username: "El_Papi1502",
+    };
+
+    const user = getUser();
+
+    expect(testUser).toEqual(user);
+  });
+
+  test("getUsusarioActivo debe de retornar un objeto", () => {
+    const name = "Pau Egea";
+    const testUser = {
+      uid: "ABC567",
+      username: name,
+    };
+
+    const user = getUsuarioActivo(name);
+
+    expect(testUser).toEqual(user);
+  });
+});
