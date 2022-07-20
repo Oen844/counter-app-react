23d683020adcbccdbe6769d728664220f93a03ca
Pau Egea <papau84@gmail.com>
2022-07-20 23:46:39 +0200

    create test component

--- src/tests/FirstApp.test.jsx
+++ src/tests/FirstApp.test.jsx
@@ -9,7 +9,23 @@ describe('Pruebas en <FristApp/>', () => {
 
         const title = 'Hola, soy Goku!';
 
-        render( <FirstApp title= {title} /> )
+        const {container} = render( <FirstApp title= {title} /> );
+
+        expect( container ).toMatchSnapshot();
+    })
+
+    test('Debe de mostrar en el titulo en una h1', () => {
+        const title = 'Hola, soy Goku!';
+
+        const { container, getByText } = render( <FirstApp title= {title} /> );
+
+        expect ( getByText(title) ).toBeTruthy();
+
+        const h1 = container.querySelector('h1');
+        console.log('**************************************')
+        console.log(h1.innerHTML);
+
+        expect(h1.innerHTML).toContain( title );
 
     })
 
